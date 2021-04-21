6175b01ce5898aeba48948279c8379d1119dbcc1
Yurak-Son <syl571@naver.com>
2021-04-21 14:49:50 +0900

    first commit

---
+++ class.py
@@ -0,0 +1,20 @@
+class A:
+    def __init__(self):
+        self._name = ''
+    
+    @property
+    def name(self) -> str:
+        return self._name
+ 
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+a = A()
+a.name = "12"
+
+a_list = []
+
+a_list.append(a)
+
+print(a)

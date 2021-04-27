3241d5a6c84ed66f7b315136b3ca90d180ac4bbc
Yurak-Son <syl571@naver.com>
2021-04-27 15:37:45 +0900

    210427

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
-        sel = i
-        for j in range(i+1,len(arr)):
-            if arr[sel]<arr[j]:
-                sel = j
-        temp = arr[sel]
-        arr[sel] = arr[i]
-        arr[i] = temp
-    return arr
-
-print(selection_sort([9,8,9,9,9,5])    )
-print(selection_sort(['9','8','9','9','9','5']))
+for i in range(10, 0,-1):
+    print(i)

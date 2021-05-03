808b6ac18f0411e7386ce310f5be28e9cfefb8a1
Yurak-Son <syl571@naver.com>
2021-05-03 17:11:49 +0900

    210503 update

--- TestCode01.py
+++ TestCode01.py
@@ -1,36 +1,39 @@
-list = [95, 99887,8,9133,9188,9,99999]
-str_list = []
+board = [[3,0,0,2],
+         [0,0,1,0],
+         [0,1,0,0],
+         [2,0,0,3]]
+#r = y
+#c = x
 
-for i in list:
-    str_list.append(str(i))
+def solution(board, r, c):
+    real_pos_value = board[r][c]
+    real_pos_y = r
+    real_pos_x = c
 
-print(str_list)
+    print("Cursor :", real_pos_y, real_pos_x, "Value :",real_pos_value)
+    ############  초기 좌표 설정 부분  ############
+    if real_pos_value == 0:
+        for x in range(4):
+            if board[real_pos_y][x] != 0:
+                real_pos_x = x
+                break
 
-def Rank_check(list):
-    for i in range(len(list)):
-        sel = i
-        str_index = 0
-        for j in range(i+1, len(list)):
-            if list[sel][str_index] < list[j][str_index]:
-                sel = j
-            try:
-                while(list[sel][str_index] == list[sel][str_index]):
-                    if list[sel][str_index] < list[j][str_index]:
-                        sel = j
-                        break
-                    elif list[sel][str_index] > list[j][str_index]:
-                        break
-                    str_index = str_index+1
-                str_index = 0
-            except :
-                print("error")
-                if len(list[sel]) > len(list[j]):
-                    sel = j
-                str_index = 0
-        temp = list[sel]
-        list[sel] = list[i]
-        list[i] = temp
+    if real_pos_value == 0:
+        for y in range(4):
+            if board[y][real_pos_x] != 0:
+                real_pos_y = y
+                break
+    # Y, X축 둘다 없을경우 초기좌표 찾아가는 함수 만들어야함
 
-    return list
+    real_pos_value = board[real_pos_y][real_pos_x]
+    print("Cursor :", real_pos_y, real_pos_x, "Value :",real_pos_value)
+    ###############################################
 
-print(Rank_check(str_list))
+    for y in range(4):
+        for x in range(4):
+            if board[y][x] == real_pos_value:
+                print("Search Value :",real_pos_value,"Searched POS :",y,x)
+
+    #return answer
+
+solution(board,0,1)
